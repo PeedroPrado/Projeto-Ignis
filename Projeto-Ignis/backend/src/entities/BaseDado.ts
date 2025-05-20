4f7a05a704e3657947185f11bf761b2b5346e345
Pedro H Prado <pedrohenriquepradodenovaes@gmail.com>
2025-05-19 22:21:30 -0300

    Add files via upload

---
+++ Projeto-Ignis/backend/src/entities/BaseDado.ts
@@ -0,0 +1,11 @@
+export interface BaseDado {
+  latitude: number;
+  longitude: number;
+  estado: string;
+  bioma: string;
+  risco_fogo: number;
+  data: string;
+  dia_sem_chuva?: string;
+  precipitacao?: number;
+  frp?: number;
+}
